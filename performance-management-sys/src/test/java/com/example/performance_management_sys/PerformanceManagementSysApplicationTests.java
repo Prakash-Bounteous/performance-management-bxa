7d4c6c4fbcbfc2226dbb6481ae487606a35d8a43
Ashish Awale <ashish.awale@bounteous.com>
2026-01-27 09:53:25 +0530

    project init

---
+++ performance-management-sys/src/test/java/com/example/performance_management_sys/PerformanceManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.performance_management_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
